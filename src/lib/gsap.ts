ef3f112c43a4376fd8d2db46fb5884c444a2a5e5
Juan Manuel Rodriguez <jmrodriguez@alliance.do>
2026-03-16 13:48:24 -0400

    feat: add GSAP library and register plugins for animations

---
+++ src/lib/gsap.ts
@@ -0,0 +1,8 @@
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { SplitText } from 'gsap/SplitText';
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, SplitText);
+
+export { gsap, ScrollToPlugin, ScrollTrigger, SplitText };
